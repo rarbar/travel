608c800207a72c10e6178ca1b17fa1c875f8366f
rarbar <senloren@yandex.ru>
2022-01-31 13:08:36 +0300

    fix project

---
+++ src/Api/travelAdvisorAPI.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { yourKeyApi } from "../key";
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+          tr_latitude: ne.lat,
+        },
+        headers: {
+          "x-rapidapi-key": yourKeyApi,
+          "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+        },
+      }
+    );
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const getWeatherData = async (lat, lng) => {
+  try {
+    if (lat && lng) {
+      const { data } = await axios.get(
+        "https://community-open-weather-map.p.rapidapi.com/find",
+        {
+          params: { lat, lon: lng },
+          headers: {
+            "x-rapidapi-key":
+              "7da463ec45msh694034f3a34e71ap10be7bjsnb16f21db69f2",
+            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+          },
+        }
+      );
+
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
